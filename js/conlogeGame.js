bf72c4c5da567d19c68b1b94f89f00983d07520f
Sergii Grynevych <grinevich.sergey.official@gmail.com>
2020-10-31 16:03:02 +0200

    Fix bug, now user need to use one of the spell after each step (in the battle with npc)

--- js/conlogeGame.js
+++ js/conlogeGame.js
@@ -16,7 +16,6 @@ class Mage extends Hero{
         this.spells = config.spells;
         this.npc = config.npc;
         this.health = config.health;
-        let npcForBattle;
     }
     
     cast() {
@@ -24,12 +23,12 @@ class Mage extends Hero{
         //     const rand = Math.floor(Math.random() * arr.length);
         //     return arr[rand];
         // };
+        objDB.indexOfSpell = Number(prompt(`Какое заклинание выберешь для атаки? ${objDB.spellsForCharacterToUse}`)) - 1;
         alert(`${this.name} кастует ${this.spells[objDB.indexOfSpell]}`);
     }
-
     npcAttack(health) {
         // console.log(`Вы встретили ${this.npc}, он агрессивно настроен и прыгает на Вас`);
-        this.cast();
+        
         let healthOfNpc = health;
         let damageToNpc = 0;
         let mageHealth = this.health;
@@ -39,6 +38,7 @@ class Mage extends Hero{
             const randomDamageByMage = Math.round(Math.random() * (100 - 1) + 1);
             const randomDamageByNpc = Math.round(Math.random() * (150 - 1) + 1);
             if (healthOfNpc >= 0) {
+                this.cast();
                 healthOfNpc -= randomDamageByMage;
                 damageToNpc += randomDamageByMage;
                 mageHealth -= randomDamageByNpc;
@@ -50,39 +50,16 @@ class Mage extends Hero{
                 }
             } 
             alert(`Вы нанесли ${randomDamageByMage} урона, у ${superMage.npcForBattle} осталось ${healthOfNpc} жизней. Вам нанесли ${damageToMage}. Остаток здоровья ${mageHealth}`);
+            
         } while (i++ < 10);
         alert(`Ты сумарно нанес ${superMage.npcForBattle}, ${Number(damageToNpc)} урона. Поздравляю с победой, ${this.name}! Остаток твоего здоровья: ${mageHealth}`);
     }
-
-    // version 1
-    //  npcAttack(health) {
-    //     console.log(`Вы встретили ${this.npc}, он агрессивно настроен и прыгает на Вас`);
-    //     this.cast();
-    //     let healthOfNpc = health;
-    //     let damageToNpc = 0;
-
-    //     let i = 0;
-    //     do {
-    //         const randomDamage = Math.round(Math.random() * (99 - 1) + 1);
-    //         if (healthOfNpc >= 0) {
-    //             healthOfNpc -= randomDamage;
-    //             damageToNpc += randomDamage;
-    //             this.cast();
-    //             console.log(`Ты сумарно нанес ${this.npc}, ${Number(damageToNpc)} урона`);
-    //             if (healthOfNpc <= 0) {
-    //                 console.log(`Вы нанесли ${randomDamage} урона, у ${this.npc} осталось ${healthOfNpc} жизней`);
-    //                 break;
-    //             }
-    //         } 
-    //         console.log(`Вы нанесли ${randomDamage} урона, у ${this.npc} осталось ${healthOfNpc} жизней`);
-    //     } while (i++ < 10);
-    // }
-
 }
 
 const objDB = {
     indexOfSpell: 0,
     ratingOfGame: 0,
+    spellsForCharacterToUse: 0,
     character: function () {
         let i = 0;
         while (i ++ < 1) {
@@ -116,25 +93,27 @@ const objDB = {
             superMage.npcForBattle = superMage.npc[0];
             alert(`В пещере, ты набрел на ${superMage.npcForBattle}. Будь осторожней, это проворные существа.`);
             // let allSpells = '';
-            const spellsForCharacterToUse = superMage.spells.map(function (spell, i) {
+            this.spellsForCharacterToUse = superMage.spells.map(function (spell, i) {
                 return ` ${ i + 1} : ${spell}`;
             });
-            this.indexOfSpell = Number(prompt(`Какое заклинание выберешь для атаки? ${spellsForCharacterToUse}`)) - 1;
-
+            // this.chooseSpell();
             // console.log(`${superMage.cast()}`);
         } else {
             superMage.npcForBattle = superMage.npc[1];
             alert(`В тёмном лесу, ты набрел на ${superMage.npcForBattle}. Будь осторожней, это проворные существа.`);
             // let allSpells = '';
-            const spellsForCharacterToUse = superMage.spells.map(function (spell, i) {
+            this.spellsForCharacterToUse = superMage.spells.map(function (spell, i) {
                 return ` ${ i + 1} : ${spell}`;
             });
-            this.indexOfSpell = Number(prompt(`Какое заклинание выберешь для атаки? ${spellsForCharacterToUse}`)) - 1;
+            this.indexOfSpell = Number(prompt(`Какое заклинание выберешь для атаки? ${this.spellsForCharacterToUse}`)) - 1;
         }
+    },
+    chooseSpell: function () {
+        do {this.indexOfSpell = Number(prompt(`Какое заклинание выберешь для атаки? ${this.spellsForCharacterToUse}`)) - 1;} while (superMage.npcAttack.healthOfNpc <= 0);
     },
     endGame: function () {
         this.ratingOfGame = Number(prompt(`Это была демо версия игры, если ты хочешь узнать как ты развивалсь события, расскажи об этой игре своим друзьям и близким, а так же не забудь оценить эту игру`, '1-100'));
-        }
+    }
 };
 // objDB.character();
 const superMage = new Mage({ name: objDB.character(), xp: 500, spells: ['огненный шар', ' морозная волна', ' ледяная глыба', ' огненная стрела', ' огненная волна'], npc: ['гоблин', 'орк'] , health: 1000 });
